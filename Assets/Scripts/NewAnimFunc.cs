b79702381f230b5dd100f71394d3caf27e90f9b0
WTS-Designer <maks.shamshurin.99@gmail.com>
2021-09-28 23:03:21 +0400

    Create_custom_GuI

--- Assets/Scripts/NewAnimFunc.cs
+++ Assets/Scripts/NewAnimFunc.cs
@@ -1,15 +1,28 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEditor;
 
 public class NewAnimFunc : MonoBehaviour
 {
-    [System.FlagsAttribute]
-    enum Func
-    {
-        Moving,
-        Rotating,
-    }
-    [SerializeField] List<int> Doing = new List<int>();
-
-    
+    public enum Action {None, Move, Rotate }
+    public Action _Action;
+    public List<Vector3> Points = new List<Vector3>();
 }
+
+#region CustomEditor
+[CustomEditor(typeof(NewAnimFunc))]
+public class CustomScript : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var myScript = target as NewAnimFunc;
+
+        myScript._Action = (NewAnimFunc.Action)EditorGUILayout.EnumPopup(myScript._Action);
+
+        if (myScript._Action == NewAnimFunc.Action.Move)
+        {
+
+        }
+    }
+}
+#endregion
