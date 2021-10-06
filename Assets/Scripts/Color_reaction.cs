6a7bd9346f7abee376e0e33e47c25f9b59ce3d1f
WTS-Designer <maks.shamshurin.99@gmail.com>
2021-10-06 21:40:00 +0400

    Almost_second_done

--- Assets/Scripts/Color_reaction.cs
+++ Assets/Scripts/Color_reaction.cs
@@ -4,16 +4,14 @@ using UnityEngine;
 
 public class Color_reaction : MonoBehaviour
 {
-    [SerializeField] private Color ToChangeColor;
-    [SerializeField] [Range(0f, 1f)] float ChangeTime;
-    private Renderer myObj;
+
+    
     private void Start()
     {
-        myObj = transform.GetComponent<Renderer>();
+        
     }
     private void FixedUpdate()
     {
-        myObj.material.color = Color.Lerp(myObj.material.color,ToChangeColor, ChangeTime*Time.deltaTime);
-
+        
     }
 }
--- Assets/Scripts/NewAnimFunc.cs
+++ Assets/Scripts/NewAnimFunc.cs
@@ -4,8 +4,17 @@ using UnityEditor;
 
 public class NewAnimFunc : MonoBehaviour
 {
-    [SerializeField] enum Action { None, Move, Rotate }
+    [SerializeField] enum Action { None, Move, Rotate, ChangeColor }
     [SerializeField] Action _Action;
+
+    #region changeColorParams
+    [SerializeField] private Color ToChangeColor;
+    [SerializeField] [Range(0f, 1f)] float ChangeTime;
+    private Renderer myObj;
+    private bool startreact = false;
+    #endregion
+
+    #region moveParams
     [SerializeField] List<Vector3> MoveTo = new List<Vector3>();
     [SerializeField] float Speed;
     [System.FlagsAttribute] enum Conditions { None, ComparePositionGO, ActivityIerarchy, }
@@ -14,19 +23,45 @@ public class NewAnimFunc : MonoBehaviour
     [SerializeField] List<GameObject> Right = new List<GameObject>(); //позиция объетка слева
     [SerializeField] List<GameObject> GOActive = new List<GameObject>();
     private int next = 0;
-    private int condIter = 0;
     private bool notdone = true;
+    private bool startMove = false;
+    #endregion
+
+    private void Start()
+    {
+        myObj = transform.GetComponent<Renderer>();
+    }
     private void FixedUpdate()
     {
         if (_Action == Action.Move)
         {
-            if (notdone && CheckCondition(Left, Right))
+            if (startMove)
             {
                 Moving();
             }
+            if (notdone && CheckCondition(Left, Right))
+            {
+                startMove = true;
+            }
+            else if (notdone == false)
+            {
+                startMove = false;
+                notdone = true;
+                next = 0;
+            }
+        }
+        if (_Action == Action.ChangeColor)
+        {
+            if (startreact)
+            {
+                Reaction();
+            }
+            if (CheckCondition(Left, Right))
+            {
+                startreact = true;
+            }
         }
     }
-
     private bool CheckCondition(List<GameObject> _left, List<GameObject> _right)
     {
         bool result = false;
@@ -39,13 +74,13 @@ public class NewAnimFunc : MonoBehaviour
         {
             result = ComparePose(_left, _right);
         }
-        if(_Conditions == Conditions.ActivityIerarchy)
+        if (_Conditions == Conditions.ActivityIerarchy)
         {
             //активность объекта в сцене
         }
-        if(_Conditions == Conditions.ActivityIerarchy && _Conditions == Conditions.ComparePositionGO)
+        if (_Conditions == Conditions.ActivityIerarchy && _Conditions == Conditions.ComparePositionGO)
         {
-            
+
         }
         return result;
     }
@@ -57,11 +92,9 @@ public class NewAnimFunc : MonoBehaviour
             if (_leftPos[i].transform.position == _rightPos[i].transform.position)
             {
                 res = true;
-                Debug.Log(i);
             }
             else
             {
-                Debug.Log("Enter");
                 res = false;
                 break;
             }
@@ -71,17 +104,23 @@ public class NewAnimFunc : MonoBehaviour
     private void Moving()
     {
         transform.position = Vector3.MoveTowards(transform.position, MoveTo[next], Speed * Time.deltaTime);
+
         if (transform.position == MoveTo[next])
         {
             next++;
         }
         if (next == MoveTo.Count) notdone = false;
     }
-
+    private void Reaction()
+    {
+        myObj.material.color = Color.Lerp(myObj.material.color, ToChangeColor, ChangeTime * Time.deltaTime);
+    }
     #region CustomEditor
     [CustomEditor(typeof(NewAnimFunc))]
     public class CustomScript : Editor
     {
+        #region MoveParams
+
         SerializedProperty MoveTo;
         SerializedProperty Speed;
         SerializedProperty _Action;
@@ -89,6 +128,9 @@ public class NewAnimFunc : MonoBehaviour
         SerializedProperty Left;
         SerializedProperty Right;
         SerializedProperty GOActive;
+        #endregion
+        SerializedProperty ToChangeColor;
+        SerializedProperty ChangeTime;
         void OnEnable()
         {
             MoveTo = serializedObject.FindProperty("MoveTo");
@@ -98,6 +140,8 @@ public class NewAnimFunc : MonoBehaviour
             Left = serializedObject.FindProperty("Left");
             Right = serializedObject.FindProperty("Right");
             GOActive = serializedObject.FindProperty("GOActive");
+            ToChangeColor = serializedObject.FindProperty("ToChangeColor");
+            ChangeTime = serializedObject.FindProperty("ChangeTime");
         }
         public override void OnInspectorGUI()
         {
@@ -109,6 +153,12 @@ public class NewAnimFunc : MonoBehaviour
             {
                 EditorGUILayout.PropertyField(MoveTo);
                 EditorGUILayout.PropertyField(Speed);
+                EditorGUILayout.PropertyField(_Conditions);
+            }
+            if (_Action.enumValueIndex == 3) //Добавление в Move поле
+            {
+                EditorGUILayout.PropertyField(ToChangeColor);
+                EditorGUILayout.PropertyField(ChangeTime);
                 EditorGUILayout.PropertyField(_Conditions);
             }
             if (_Conditions.enumValueIndex == 1)
