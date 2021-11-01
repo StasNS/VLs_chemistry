05138feb2285af964dae74c28f21bb396bf1e314
WTS-Designer <maks.shamshurin.99@gmail.com>
2021-11-01 11:23:53 +0400

    Some_work

--- Assets/Scripts/Desrciption.cs
+++ Assets/Scripts/Desrciption.cs
@@ -6,15 +6,25 @@ using TMPro;
 public class Desrciption : MonoBehaviour
 {
     [SerializeField] private GameObject description;
+    Vector3 beforepos = new Vector3(0, 0, 0);
     private void OnMouseOver()
     {
-        description.transform.localScale = new Vector3(-1,1,1);
+        description.transform.localScale = new Vector3(-1, 1, 1);
         description.transform.LookAt(Camera.main.transform);
         description.SetActive(true);
     }
     private void OnMouseExit()
     {
-        description.transform.localScale = new Vector3(1,1,1);
+        description.transform.localScale = new Vector3(1, 1, 1);
         description.SetActive(false);
+    }
+
+    private void FixedUpdate()
+    {
+        if (transform.position != beforepos)
+        {
+            description.SetActive(false);
+            beforepos = transform.position;
+        }
     }
 }
---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Splash : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            
+        }
+    }
+}
