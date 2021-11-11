87e70cdd389c6deda99867267ce75a97ae996543
WTS-Designer <maks.shamshurin.99@gmail.com>
2021-11-11 18:23:42 +0400

    Work

--- Assets/Scripts/Splash.cs
+++ Assets/Scripts/Splash.cs
@@ -4,9 +4,45 @@ using UnityEngine;
 
 public class Splash : MonoBehaviour
 {
+    [SerializeField] private GameObject prob;
+    [SerializeField] private float Speed;
+    [SerializeField] private GameObject place_Prob;
+    [SerializeField] private GameObject aDrop;
+    [SerializeField] private Vector3 rot_Angle;
+    private Quaternion cur_Angle;
+    private bool start = false;
+    private bool start = false;
     private void FixedUpdate()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (prob.transform.position == place_Prob.transform.position)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); //луч триггер
+                RaycastHit hit; //регистрация попадания
+
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity, 1, QueryTriggerInteraction.Ignore))
+                {
+                    if (hit.transform.gameObject == GameObject.Find("Smesitel"))
+                    {
+                        Debug.Log("Got");
+                        cur_Angle = prob.transform.rotation;
+                        start = true;
+                    }
+                }
+            }
+        }
+        if (start)
+        {
+            Splasing(cur_Angle);
+        }
+    }
+    private void Splasing(Quaternion back_angle)
+    {
+        Debug.Log("wow");
+        var _angle = Quaternion.Euler(rot_Angle);
+        prob.transform.rotation = Quaternion.Lerp(prob.transform.rotation, _angle, Speed * Time.deltaTime);
+        if (prob.transform.rotation.eulerAngles.ToString() == rot_Angle.ToString())
         {
             
         }
