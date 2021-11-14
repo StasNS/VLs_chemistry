2b8949593ee40f5a9e1f0489a228da59edf69fc6
WTS-Designer <maks.shamshurin.99@gmail.com>
2021-11-14 17:42:19 +0400

    Hard work

--- Assets/Scripts/ReactionSplash.cs
+++ Assets/Scripts/ReactionSplash.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,23 +6,43 @@ using UnityEngine;
 public class ReactionSplash : MonoBehaviour
 {
     [SerializeField] GameObject blob;
+    [SerializeField] int timeofReact;
+    [SerializeField] Vector3 blobTarget;
+    [SerializeField] GameObject Kolba;
+    [SerializeField] int CountofBlob;
     [SerializeField] float step;
     private bool react = false;
+    private bool flag = false;
+    private float iterCount = 0;
+    private float timer = 0;
 
     private void FixedUpdate()
     {
-        if (blob.transform.position.ToString() == new Vector3(-0.728f, 1.4f, 0.3089f).ToString())
+        if (blob.transform.position.ToString() == blobTarget.ToString())
         {
-            react = true;
+            iterCount++;
+            if ((iterCount - 55) == CountofBlob)
+            {
+                react = true;
+            }
         }
         if (react)
         {
-            var getfiller = GameObject.Find("Kolba_Sulfur").GetComponent<Renderer>().material.GetFloat("noise_vect");
-            var setfiller = getfiller - step * Time.deltaTime;
-            GameObject.Find("Kolba_Sulfur").GetComponent<Renderer>().material.SetFloat("noise_vect", setfiller);
-            var getfillerdel = GameObject.Find("Sphere").GetComponent<Renderer>().material.GetFloat("noise_vect");
-            var setfillerdel = getfillerdel - step * Time.deltaTime;
-            GameObject.Find("Sphere").GetComponent<Renderer>().material.SetFloat("noise_vect", setfillerdel);
+            timer += Time.deltaTime;
+            if (timeofReact == Convert.ToInt32(timer))
+            {
+                flag = true;
+            }
+            if (flag)
+            {
+                var getfiller = Kolba.GetComponent<Renderer>().material.GetFloat("noise_vect");
+                var setfiller = getfiller - step * Time.deltaTime;
+                Kolba.GetComponent<Renderer>().material.SetFloat("noise_vect", setfiller);
+                if(Convert.ToInt32(setfiller) == 0)
+                {
+                    flag = false;
+                }
+            }
         }
     }
 }
--- Assets/Scripts/Splash.cs
+++ Assets/Scripts/Splash.cs
@@ -5,34 +5,52 @@ using UnityEngine;
 public class Splash : MonoBehaviour
 {
     [SerializeField] private GameObject prob;
-    [SerializeField] private GameObject kolbaForTube;
+    [SerializeField] private List<GameObject> kolbaForTube = new List<GameObject>();
     [SerializeField] private float speed;
     [SerializeField] private float blobspeed;
-    [SerializeField] private GameObject place_Prob;
-    [SerializeField] private GameObject blob;
-    [SerializeField] private Vector3 blobTarget;
+    [SerializeField] private List<GameObject> place_Prob = new List<GameObject>();
+    [SerializeField] private List<GameObject> blob = new List<GameObject>();
+    [SerializeField] private List<Vector3> blobTarget = new List<Vector3>();
     [SerializeField] private Vector3 rot_Angle;
     private Quaternion startedAngle = new Quaternion(-0.5f, -0.5f, -0.5f, 0.5f);
     private bool there = false;
     private bool back = false;
-    private Vector3 blobPosStart;
+    int i = 0;
+    private List<Vector3> blobPosStart;
     private void Start()
     {
-        blobPosStart = blob.transform.position;
+        blobPosStart = new List<Vector3>() {blob[0].transform.position,blob[1].transform.position,blob[2].transform.position};
     }
     private void FixedUpdate()
     {
-        if (prob.transform.position == place_Prob.transform.position)
+        if (prob.transform.position == place_Prob[0].transform.position ||
+        prob.transform.position == place_Prob[1].transform.position ||
+        prob.transform.position == place_Prob[2].transform.position)
         {
             if (Input.GetMouseButtonDown(0))
             {
+                
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 RaycastHit hit;
 
                 if (Physics.Raycast(ray, out hit, Mathf.Infinity, 1, QueryTriggerInteraction.Ignore))
                 {
-                    if (hit.transform.gameObject == kolbaForTube)
+                    if (hit.transform.gameObject == kolbaForTube[0] ||
+                    hit.transform.gameObject == kolbaForTube[1] ||
+                    hit.transform.gameObject == kolbaForTube[2])
                     {
+                        if (hit.transform.gameObject == kolbaForTube[0])
+                        {
+                            i = 0;
+                        }
+                        if (hit.transform.gameObject == kolbaForTube[1])
+                        {
+                            i = 1;
+                        }
+                        if (hit.transform.gameObject == kolbaForTube[2])
+                        {
+                            i = 2;
+                        }
                         there = true;
                     }
                 }
@@ -40,29 +58,47 @@ public class Splash : MonoBehaviour
         }
         if (there)
         {
-            Splasing();
+            Splasing(i);
         }
         if (back)
         {
             Reverse(startedAngle);
         }
-        if (blob.activeInHierarchy)
+        if (blob[0].activeInHierarchy)
         {
-            blob.transform.position = Vector3.MoveTowards(blob.transform.position, blobTarget, blobspeed * Time.deltaTime);
-            if (blob.transform.position == blobTarget)
+            blob[0].transform.position = Vector3.MoveTowards(blob[0].transform.position, blobTarget[0], blobspeed * Time.deltaTime);
+            if (blob[0].transform.position == blobTarget[0])
             {
-                blob.SetActive(false);
-                blob.transform.position = blobPosStart;
+                blob[0].SetActive(false);
+                blob[0].transform.position = blobPosStart[0];
+            }
+        }
+        if (blob[1].activeInHierarchy)
+        {
+            blob[1].transform.position = Vector3.MoveTowards(blob[1].transform.position, blobTarget[1], blobspeed * Time.deltaTime);
+            if (blob[1].transform.position == blobTarget[1])
+            {
+                blob[1].SetActive(false);
+                blob[1].transform.position = blobPosStart[1];
+            }
+        }
+        if (blob[2].activeInHierarchy)
+        {
+            blob[2].transform.position = Vector3.MoveTowards(blob[2].transform.position, blobTarget[2], blobspeed * Time.deltaTime);
+            if (blob[1].transform.position == blobTarget[2])
+            {
+                blob[2].SetActive(false);
+                blob[2].transform.position = blobPosStart[2];
             }
         }
     }
-    private void Splasing()
+    private void Splasing(int i)
     {
         var _angle = Quaternion.Euler(rot_Angle);
         prob.transform.rotation = Quaternion.Lerp(prob.transform.rotation, _angle, speed * Time.deltaTime);
         if (prob.transform.rotation.eulerAngles.ToString() == rot_Angle.ToString())
         {
-            blob.SetActive(true);
+            blob[i].SetActive(true);
             back = true;
             there = false;
         }
